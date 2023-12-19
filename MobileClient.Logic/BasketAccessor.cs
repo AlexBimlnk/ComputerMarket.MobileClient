7f0f53f942a9a2411cc2225801ef81e58cc61b6c
centuriin <centuriin@yandex.ru>
2023-12-19 18:46:47 +0300

    #9 cleunup

--- MobileClient.Logic/BasketAccessor.cs
+++ MobileClient.Logic/BasketAccessor.cs
@@ -1,12 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Extensions.Options;
 
-using Microsoft.Extensions.Options;
-
-using MobileClient.Contract.AccountController;
 using MobileClient.Contract.BasketController;
 using MobileClient.Logic.Configuration;
 using MobileClient.Logic.Transport;
@@ -17,21 +10,15 @@ public sealed class BasketAccessor : IBasketAccessor
 {
     private readonly IHttpClientFacade _httpClientFacade;
     private readonly ServiceConfig _serviceConfig;
-    private readonly ISerializer<Login, string> _loginSerializer;
-    private readonly ISerializer<Register, string> _registerSerializer;
     private readonly IDeserializer<HttpResponseMessage, IReadOnlyCollection<PurchasableEntity>> _basketDeserializer;
 
     public BasketAccessor(
         IHttpClientFacade httpClientFacade,
-        ISerializer<Login, string> loginSerializer,
-        ISerializer<Register, string> registerSerializer,
         IDeserializer<HttpResponseMessage, IReadOnlyCollection<PurchasableEntity>> deserializer,
         IOptions<ServiceConfig> options)
     {
         _httpClientFacade = httpClientFacade ?? throw new ArgumentNullException(nameof(httpClientFacade));
         _serviceConfig = options.Value ?? throw new ArgumentNullException(nameof(options));
-        _loginSerializer = loginSerializer ?? throw new ArgumentNullException(nameof(loginSerializer));
-        _registerSerializer = registerSerializer ?? throw new ArgumentNullException(nameof(registerSerializer));
         _basketDeserializer = deserializer ?? throw new ArgumentNullException(nameof(deserializer));
     }
 
