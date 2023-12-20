be33233590c891e932a3e916ea087a00b7db6dd7
centuriin <centuriin@yandex.ru>
2023-12-20 19:25:56 +0300

    #8 add missed json prop

--- MobileClient.Contract/Reports/Report.cs
+++ MobileClient.Contract/Reports/Report.cs
@@ -1,23 +1,29 @@
-﻿namespace MobileClient.Contract.Reports;
+﻿using Newtonsoft.Json;
+
+namespace MobileClient.Contract.Reports;
 public sealed class Report
 {
     /// <summary>
     /// Поставщик по которому составляется отчет.
     /// </summary>
+    [JsonProperty("provider")]
     public Provider Provider { get; }
 
     /// <summary>
     /// Кол-во проданных продуктов.
     /// </summary>
+    [JsonProperty("soldProductsCount")]
     public long SoldProductsCount { get; }
 
     /// <summary>
     /// Самый продоваемый продукт.
     /// </summary>
+    [JsonProperty("productMVP")]
     public Product ProductMVP { get; }
 
     /// <summary>
     /// Общая прибыль.
     /// </summary>
+    [JsonProperty("totalProfit")]
     public decimal TotalProfit { get; }
 }
