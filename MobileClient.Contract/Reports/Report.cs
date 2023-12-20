f9ae753b91f4f6b8bd451955269cc5318e976ae1
centuriin <centuriin@yandex.ru>
2023-12-20 19:26:25 +0300

    #8 add public setter

--- MobileClient.Contract/Reports/Report.cs
+++ MobileClient.Contract/Reports/Report.cs
@@ -7,23 +7,23 @@ public sealed class Report
     /// Поставщик по которому составляется отчет.
     /// </summary>
     [JsonProperty("provider")]
-    public Provider Provider { get; }
+    public Provider Provider { get; set; }
 
     /// <summary>
     /// Кол-во проданных продуктов.
     /// </summary>
     [JsonProperty("soldProductsCount")]
-    public long SoldProductsCount { get; }
+    public long SoldProductsCount { get; set; }
 
     /// <summary>
     /// Самый продоваемый продукт.
     /// </summary>
     [JsonProperty("productMVP")]
-    public Product ProductMVP { get; }
+    public Product ProductMVP { get; set; }
 
     /// <summary>
     /// Общая прибыль.
     /// </summary>
     [JsonProperty("totalProfit")]
-    public decimal TotalProfit { get; }
+    public decimal TotalProfit { get; set; }
 }
